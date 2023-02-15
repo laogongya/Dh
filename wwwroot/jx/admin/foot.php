c65cf10bac3b5239ae5a5bf8c9001134c4508d9d
laogongya <125042805+laogongya@users.noreply.github.com>
2023-02-15 16:18:15 +0800

    Delete wwwroot/jx directory

--- wwwroot/jx/admin/foot.php
+++
@@ -1,3 +0,0 @@
-<div id="footer"><a href="http://www.noome.cn" target="_blank" style="color:#999999">Copyright © 2018-2020版权所有</span>
-			<br/></a>
-</div> 
--- wwwroot/jx/admin/inc.php
+++
@@ -1,5 +0,0 @@
-<title>VIP视解析管理后台</title>
-<meta name="keywords" content="VIP影视解析平台" />
-<meta name="description" content="VIP影视解析平台" />
-<link href="./images/admin.css" rel="stylesheet" type="text/css" />
-<link rel="shortcut icon" type="image/x-icon" href="./../favicon.ico"><?php 
